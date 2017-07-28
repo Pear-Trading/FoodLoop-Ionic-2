fba4976cdc47cf71b2bd1d93cb8e844eaa637f84
Finn <toyou1995@gmail.com>
2017-07-28 13:51:37 +0100

    Increased length of toast popup

--- src/pages/receipt/receipt.ts
+++ src/pages/receipt/receipt.ts
@@ -326,7 +326,7 @@ export class ReceiptPage {
   private presentToast(text) {
     let toast = this.toastCtrl.create({
       message: text,
-      duration: 3000,
+      duration: 6000,
       position: 'top'
     });
     toast.present();
