6361b81ab726f2f945fd39852270f4feed3a20d9
piratefinn <toyou1995@gmail.com>
2017-10-09 13:15:51 +0100

    Refactoring

--- src/providers/user-data.ts
+++ src/providers/user-data.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Events } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
